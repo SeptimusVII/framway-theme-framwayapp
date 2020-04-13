a3c0be1225cf837f9713b8f985062464c9454107
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-04-13 12:11:44 +0200

    add app style adn basic script

--- framwayapp.js
+++ framwayapp.js
@@ -1,3 +1,20 @@
 $(function(){
 
 });
+
+app.addCustomEventListener = function (selector, event, handler) {
+    let rootElement = document.querySelector('body');
+    //since the root element is set to be body for our current dealings
+    rootElement.addEventListener(event, function (evt) {
+            var targetElement = evt.target;
+            while (targetElement != null) {
+                if (targetElement.matches(selector)) {
+                    handler(evt);
+                    return;
+                }
+                targetElement = targetElement.parentElement;
+            }
+        },
+        true
+    );
+}
